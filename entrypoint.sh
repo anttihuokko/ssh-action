42c3cc2a0a0fc1291657be23eb45da5581e158df
anttihuokko <150904007+anttihuokko@users.noreply.github.com>
2023-11-21 17:52:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,5 +64,12 @@ echo "  User $USER" >> $SSH_DIR/config
 
 chmod 600 $SSH_DIR/*
 
-echo Starting to execute ssh command
+
+
+
+echo "Host key: $HOST_KEY"
+
+
+
+echo '--- Starting to execute ssh command ---'
 ssh target $COMMAND
